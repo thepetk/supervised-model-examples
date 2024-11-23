9519b8f17e84ea98216eeca06363927a116651f0
thepetk <thepetk@gmail.com>
2024-11-23 08:34:51 -0500

    Further alignment with the assignment
    Signed-off-by: thepetk <thepetk@gmail.com>

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Callable
 import numpy as np
 
 DATA_TEST_PATH = "data_test.csv"
@@ -13,7 +13,7 @@ VALIDATION_RATIO = 0.15
 
 
 # activation methods (relu + relu_derivative)
-def fact(x: "np.ndarray") -> "np.ndarray":
+def relu(x: "np.ndarray") -> "np.ndarray":
     return np.maximum(0, x)
 
 
@@ -22,9 +22,28 @@ def fact_derivative(x: "np.ndarray") -> "np.ndarray":
 
 
 class NeuralNet:
-    def __init__(self, layers: "list[int]") -> None:
-        self.L = len(layers)
-        self.n = layers.copy()
+    def __init__(
+        self,
+        total_layers: "int",
+        units_per_layer: "list[int]",
+        learning_rate: "float",
+        fact: "Callable[...]",
+        validation_percentage: "float",
+        num_of_epochs: "int",
+    ) -> None:
+        self.L = total_layers
+        self.n = units_per_layer
+        # the total layers should be equal to the length of units per layer
+        assert self.L == len(
+            self.n
+        ), "total layers should be equivalent to the length of units_per_layer"
+
+        # initialize learning rate, activation function, validation percentage
+        # and number of epochs
+        self.learning_rate = learning_rate
+        self.fact = fact
+        self.validation_percentage = validation_percentage
+        self.num_of_epochs = num_of_epochs
 
         # initialize all neurons for all given layers
         self.xi: "list[np.ndarray]" = []
@@ -35,20 +54,20 @@ class NeuralNet:
         self.w: "list[np.ndarray]" = []
         self.theta: "list[np.ndarray]" = []
         for layer in range(1, self.L):
-            self.w.append(np.random.randn(layers[layer], layers[layer - 1]))
+            self.w.append(np.random.randn(self.n[layer], self.n[layer - 1]))
             self.theta.append(np.random.randn(self.n[layer], 1))
 
-    def forward(self, x: "np.ndarray") -> "np.ndarray":
+    def _forward(self, x: "np.ndarray") -> "np.ndarray":
         """
         performs feed forward propagation for a given x train data array
         """
         self.xi[0] = x.reshape(-1, 1)
         for layer in range(1, self.L):
             h = self.w[layer - 1] @ self.xi[layer - 1] - self.theta[layer - 1]
-            self.xi[layer] = fact(h)
+            self.xi[layer] = self.fact(h)
         return self.xi[-1]
 
-    def backward(self, y: "np.ndarray") -> "tuple[np.ndarray, np.ndarray]":
+    def _backward(self, y: "np.ndarray") -> "tuple[np.ndarray, np.ndarray]":
         """
         performs back propagation for a given y array with the result
         """
@@ -66,23 +85,24 @@ class NeuralNet:
 
         return d_w, d_theta
 
-    def update_weights(
+    def _update_weights(
         self,
         d_w: "list[np.ndarray]",
         d_theta: "list[np.ndarray]",
-        learning_rate: "float",
     ) -> None:
         for layer in range(1, self.L):
             d_w_prev = d_w[layer]
             d_theta_prev = d_theta[layer]
-            self.w[layer - 1] += learning_rate * d_w_prev
-            self.theta[layer - 1] += learning_rate * d_theta_prev
+            self.w[layer - 1] += self.learning_rate * d_w_prev
+            self.theta[layer - 1] += self.learning_rate * d_theta_prev
 
-    def compute_mean_square_error(self, data: "np.ndarray", y: "np.ndarray") -> Any:
+    def _compute_mean_square_error(
+        self, data: "np.ndarray", y: "np.ndarray"
+    ) -> "float":
         error = 0.0
         total_records, _ = data.shape
         for i in range(total_records):
-            pred = self.forward(data[i])
+            pred = self._forward(data[i])
             error += np.sum((pred - y[i]) ** 2)
         return error / total_records
 
@@ -90,39 +110,49 @@ class NeuralNet:
         predictions = []
         total_records, _ = data.shape
         for i in range(total_records):
-            predictions.append(self.forward(data[i]))
+            predictions.append(self._forward(data[i]))
         return np.array(predictions)
 
-    def train(
+    def _split_data(
+        self, data: "np.ndarray", total_records: "int"
+    ) -> "tuple[np.ndarray, np.ndarray]":
+        """
+        splits the data into validation and training according to the give
+        validation percentage
+        """
+        _val_splitter = int(self.validation_percentage * total_records)
+
+        return data[_val_splitter:], data[:_val_splitter]
+
+    def fit(
         self,
-        data_train: "np.ndarray",
-        y_train: "np.ndarray",
-        data_val: "np.ndarray",
-        y_val: "np.ndarray",
-        epochs: "int",
-        learning_rate: "float",
+        data: "np.ndarray",
+        y: "np.ndarray",
     ) -> None:
         # Training the network with mini-batch stochastic gradient descent
-        total_records, _ = data_train.shape
-        for epoch in range(epochs):
-            for _ in range(total_records):
+        original_total_records, _ = data.shape
+        # Split output train into train and validation
+        y_train, y_val = self._split_data(y, original_total_records)
+        data_train, data_val = self._split_data(data, original_total_records)
+        for epoch in range(self.num_of_epochs):
+            for _ in range(data_train.shape[0]):
                 # select a random item from the records
-                m = np.random.randint(total_records)
+                m = np.random.randint(data_train.shape[0])
 
                 # Run the forward propagation
-                self.forward(data_train[m])
+                self._forward(data_train[m])
 
                 # Get the weight and threshold changes after backward propagation
-                d_w, d_theta = self.backward(y_train[m])
+                d_w, d_theta = self._backward(y_train[m])
 
                 # Adjust weight according to results of training
-                self.update_weights(d_w, d_theta, learning_rate)
+                self._update_weights(d_w, d_theta)
 
             # After an epoch is finished calculate the error for training and validation sets
-            train_error = self.compute_mean_square_error(data_train, y_train)
-            val_error = self.compute_mean_square_error(data_val, y_val)
+            train_error = self._compute_mean_square_error(data_train, y_train)
+            val_error = self._compute_mean_square_error(data_val, y_val)
 
-            if epoch % 10 == 0 or epoch == epochs - 1:
+            if epoch % 10 == 0 or epoch == self.num_of_epochs - 1:
                 print(
                     f"Epoch {epoch}, Train Error: {train_error:.6f}, Val Error: {val_error:.6f}"
                 )
@@ -134,36 +164,24 @@ if __name__ == "__main__":
     test_data = np.genfromtxt(DATA_TEST_PATH, delimiter=",")
     test_y = np.genfromtxt(RESULTS_TEST_PATH, delimiter=",")
 
-    # Get the total number of records
-    total_records, _ = data_train.shape
-
-    # Define a split between validation and training
-    val_splitter = int(VALIDATION_RATIO * total_records)
-
-    # Split data train into train and validation
-    data_train = data_train[val_splitter:]
-    data_val = data_train[:val_splitter]
-
-    # Split output train into train and validation
-    y_train = y_train[val_splitter:]
-    y_val = y_train[:val_splitter]
-
     # Get the total number of features
     _, total_features = data_train.shape
 
     # Initialize the neural network
     total_layers = [total_features] + HIDDEN_LAYERS + OUTPUT_LAYER
-    neuronet = NeuralNet(layers=total_layers)
+    neuronet = NeuralNet(
+        total_layers=len(total_layers),
+        units_per_layer=total_layers,
+        learning_rate=LEARNING_RATE,
+        fact=relu,
+        validation_percentage=VALIDATION_RATIO,
+        num_of_epochs=NUM_OF_EPOCHS,
+    )
 
-    print(y_train[0])
     # Train the neural network
-    neuronet.train(
+    neuronet.fit(
         data_train,
         y_train,
-        data_val,
-        y_val,
-        epochs=NUM_OF_EPOCHS,
-        learning_rate=LEARNING_RATE,
     )
 
     # After training we are ready to test
